f4c63844d90d73e329ab0e90dacf553f7fa22844
Casey Marshall <cmarshall@lyveminds.com>
2014-11-13 13:41:37 -0800

    More hostile-network-friendly.
    * Lookup the DNS name after 60 seconds (mostly informed by AWS's guidance here).
    * Look up all addresses associated with a name, and round-robin through them (again, thanks to AWS and how ELBs work).
    * Use SocketChannel, check that we wrote the whole buffer.
    * If we fail to send a message, *try again* one time (otherwise, if your messages were widely spaced enough, wouldn't you *never* send a message?)

--- src/main/java/org/graylog2/GelfTCPSender.java
+++ src/main/java/org/graylog2/GelfTCPSender.java
@@ -3,55 +3,102 @@ package org.graylog2;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.security.Security;
+import java.util.concurrent.TimeUnit;
 
 public class GelfTCPSender implements GelfSender {
 	private boolean shutdown = false;
-	private InetAddress host;
+    private String host;
+	private InetAddress[] hosts;
+    private int hostIndex;
 	private int port;
-	private Socket socket;
-    private OutputStream os;
+    private SocketChannel channel;
+    private long lastLookupTime;
+    private int errorMessages = 5;
 
     public GelfTCPSender() {
     }
 
 	public GelfTCPSender(String host, int port) throws IOException {
-		this.host = InetAddress.getByName(host);
+        this.host = host;
 		this.port = port;
-		this.socket = new Socket(host, port);
-        this.os = socket.getOutputStream();
+        lookup();
+        connect();
 	}
 
 	public boolean sendMessage(GelfMessage message) {
+        return sendMessageWithRetry(message, true);
+    }
+
+    private boolean sendMessageWithRetry(GelfMessage message, boolean retry) {
 		if (shutdown || !message.isValid()) {
+            if (errorMessages > 0) {
+                System.err.printf("not sending message, shutdown:%s valid?:%s message:%s", shutdown, message.isValid(), message);
+                errorMessages--;
+            }
 			return false;
 		}
 
-		try {
-			// reconnect if necessary
-			if (socket == null || os == null) {
-				socket = new Socket(host, port);
-                os = socket.getOutputStream();
-			}
+        try {
+            // Look up the DNS name again, if > 60 seconds.
+            if (System.currentTimeMillis() - lastLookupTime > TimeUnit.MILLISECONDS.convert(60, TimeUnit.SECONDS)) {
+                lookup();
+                connect();
+            }
 
-            os.write(message.toTCPBuffer().array());
+			// reconnect if necessary
+            if (channel == null || channel.isConnected()) {
+                connect();
+            }
+
+            ByteBuffer messageBuffer = message.toTCPBuffer();
+            int remaining = messageBuffer.remaining();
+            int written = channel.write(message.toTCPBuffer());
+            if (written != remaining) {
+                throw new IOException(String.format("short write, expected to write %d, wrote %d", remaining, written));
+            }
 
 			return true;
 		} catch (IOException e) {
-			// if an error occours, signal failure
-			socket = null;
+			// if an error occurs, signal failure
+            if (errorMessages > 0) {
+                System.err.printf("failed sending message");
+                e.printStackTrace();
+                errorMessages++;
+            }
+            try {
+                channel.close();
+            } catch (IOException e1) {
+                // ignore
+            }
+            channel = null;
+            if (retry) {
+                return sendMessageWithRetry(message, false);
+            }
 			return false;
 		}
 	}
+
+    private void connect() throws IOException {
+        this.channel = SocketChannel.open(new InetSocketAddress(hosts[hostIndex], port));
+        hostIndex = (hostIndex + 1) % hosts.length;
+    }
+
+    private void lookup() throws UnknownHostException {
+        this.hosts = InetAddress.getAllByName(this.host);
+        this.lastLookupTime = System.currentTimeMillis();
+        hostIndex = 0;
+    }
 
 	public void close() {
 		shutdown = true;
 		try {
-            if (os != null){
-                os.close();
+            if (channel != null) {
+                channel.close();
+                channel = null;
             }
-			if (socket != null) {
-				socket.close();
-			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
